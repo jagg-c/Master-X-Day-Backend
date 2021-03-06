8e550f501f272a7db78ef4c36a9518c290b43d9b
Alejandro Sánchez Yalí <alejandro.driveyali@gmail.com>
2021-03-06 15:31:12 -0500

    feat: routes definition

--- api/routes/directory.js
+++ api/routes/directory.js
@@ -4,4 +4,13 @@ const express = require('express')
 
 const router = express.Router()
 
+router.route('/flight/')
+    .get()
+    .post()
+    .put()
+    .delete()
+
+
+
 module.exports = router
+
