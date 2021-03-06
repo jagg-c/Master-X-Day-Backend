070e8685a54e1e3939a0172203a0494f02d53c63
Jhony Alexander Garcia Gómez <jhonyagarcia@gmail.com>
2021-03-06 16:58:08 -0500

    fix: body parser fixed

--- api/index.js
+++ api/index.js
@@ -5,11 +5,13 @@ const bodyParser = require("body-parser")
 const routes = require('../api/routes/directory')
 const expressJSDocSwagger = require("express-jsdoc-swagger")
 
-
 const app = express()
 
 const PORT = 3000
 
+app.use(bodyParser.json())
+
 app.use('/', routes)
-app.use(bodyParser.urlencoded({ extended: false }))
-app.listen(process.env.PORT, () => console.log(`App listening at http:/localhost:${PORT}`))
+
+
+app.listen(PORT, () => console.log(`App listening at http:/localhost:${PORT}`))
