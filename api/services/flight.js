2659ae9d60b01d6f0220542de268ed838120517d
Jhony Alexander Garcia Gómez <jhonyagarcia@gmail.com>
2021-03-06 16:57:24 -0500

    feat: integrated service with route

--- api/services/flight.js
+++ api/services/flight.js
@@ -6,27 +6,27 @@ class FlightService {
   }
 
   async getFlight ({ flightId }){
-    const flight = await this.PostgresLib.get(this.collection, flightId);
+    const flight = [] //await this.PostgresLib.get(this.collection, flightId);
     return flight || [];
   }
 
-  async getFlights ({}){
-    const flights = await this.PostgresLib.getAll(this.collection);
+  async getFlights (){
+    const flights = []; //await this.PostgresLib.getAll(this.collection);
     return flights || [];
   }
 
   async createFlight ({ flight }){
-    const createdFlightId = await this.PostgresLib.insert(this.collection, flight);
+    const createdFlightId = 1; //await this.PostgresLib.insert(this.collection, flight);
     return createdFlightId;
   }
 
   async updateFlight ({ flightId,  flight }){
-    const createdFlightId = await this.PostgresLib.update(this.collection, flightId, flight);
-    return createdFlightId;
+    const updatedFlightId = flightId //;await this.PostgresLib.update(this.collection, flightId, flight);
+    return updatedFlightId;
   }
 
   async deleteFlight({ flightId }){
-    const deletedFlightId = await this.PostgresLib.delete(this.collection, flightId);
+    const deletedFlightId = flightId; //await this.PostgresLib.delete(this.collection, flightId);
     return deletedFlightId;
   }
 }
